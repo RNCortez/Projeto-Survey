8889e38c1f90c616588ae0c9f6019cb2be9e3193
Rafael Cortez <rafael.cortez@hotmail.com>
2019-03-09 16:48:51 -0300

    Add controle Dashboad, questionarios, home e criado suas views

---
+++ SurveyWeb/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SurveyWeb.Filters;
+
+namespace SurveyWeb.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        [ValidarAcesso]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SurveyWeb/Controllers/HomeController.cs
+++ SurveyWeb/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SurveyWeb.Controllers
@@ -11,6 +12,39 @@ namespace SurveyWeb.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Validar(IFormCollection form)
+        {
+            int usuario = 0;
+            string senha = "";
+
+            int.TryParse(form["txtEmail"], out usuario);
+            senha = form["txtSenha"];
+
+            if (usuario == 1000 && senha == "123")
+            {
+                CookieOptions ck = new CookieOptions();
+                ck.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Append("usuario", usuario.ToString(), ck);
+                Response.Cookies.Append("nivel", "A", ck);
+
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                ViewBag.Erro = "Usuário inválido.";
+                return View("Index");
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("usuario");
+            Response.Cookies.Delete("nivel");
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
